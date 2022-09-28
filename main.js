b224ff665a385c4f46376ca7d992a9f81613d8c4
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-28 17:47:45 -0500

    Ejercicio 1

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 addEventListener("DOMContentLoaded",()=>{
 ///1. en una granja se requiere saber alguna informacion para determinar el precio de venta por cada kilo de huevo. precio se determina a traves del promedio de calidad de las N gallinas que hay en la granja. La calidad de cada gallina se obtiene segun la formula  ..........calidad = (peso gallina*alturagallina)/Numero de huevos que pone
+var preciVenta 
 let ac = 0
 let n = 0
 let n1 = Number(prompt('ingrese la cantidad N de gallinas.', ))
@@ -9,8 +10,10 @@ while(n < n1){
     let alturaGallina = Number(prompt('Ingrese la altura de la gallina '))
     let numerHuveos = Number(prompt('Ingrese la cantidad de huevos que pone '))
     let calidad = (pesoGallinas*alturaGallina)/numerHuveos
+    let a = ac+=calidad
+    preciVenta = a/n1
     console.log(`El numero de calidad de la gallina es : ${calidad}`);
 }
-//console.log(`${ac}`);
+console.log(`el precio de las gallinas es : ${preciVenta}`);
 
 })
