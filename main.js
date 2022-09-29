e5841b21f4834b35260f48604e2e1b6b89508ad4
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-28 20:00:37 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 addEventListener("DOMContentLoaded",()=>{
-    // 3. Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso de
-    // computación.
-    let n = 0
-    acum = 0
-    var promedio
-    while (n<5) {
+    // 4. 42 personas toman parte de un baile. Durante la fiesta una dama bailó con 7 caballeros, una segunda
+    // dama con 8, una tercera con 9, y así sucesivamente hasta que la última bailó con todos los hombres.
+    // Cuantas damas había en el baile?
+    var n = 6
+    while (n < 42){
         n++
-        let nombre = (prompt('ingrese su nombre'))
-        let apellido = (prompt('Ingrese su apellido'))
-        let edad = Number(prompt('Ingrese su edad'))
-        let nota = Number(prompt('Ingrese su nota'))
-        acum += nota
-        promedio = acum/2
-        console.log(`${nombre}`)
-        console.log(`${apellido}`)
-        console.log(`${edad}`)
-        console.log(`${nota}`)
-        console.log(`----------------------------`)
-
+        console.log(`n`)
+        
     }
-console.warn(`El promedio de las notas de los estudiantes es : ${promedio}`)
+    let numeroPersonas = 36/2
+    let caballeros = 42 - numeroPersonas
+    console.warn(`El numero de mujeres es  "${numeroPersonas}" y el numero de caballeros es "${caballeros}"`)
 })
