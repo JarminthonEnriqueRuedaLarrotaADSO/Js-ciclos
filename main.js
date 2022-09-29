be008520692fd5646b98f6fae47c83a0bb63c5ea
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-28 21:50:24 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 addEventListener("DOMContentLoaded",()=>{
-    //7. Una compañía de seguros tiene contratados a n vendedores. Cada uno hace tres ventas a la semana. Su política de pagos es que un vendedor recibe un sueldo base, y un 10% extra por comisiones de
-    // sus ventas.El gerente de su compañía desea saber cuanto dinero obtendrá en la semana cada vendedor por concepto de comisiones por las tres ventas realizadas, y cuanto tomando en cuenta su sueldo base y sus comisiones.
-    let n1 = Number(prompt('ingrese le numero de vendedores '))
-    let n = 0
-    while (n<n1) {
-        n++
-        let pago1 = Number(prompt('Ingrese el valor de la primera venta '))
-        let pago2 = Number(prompt('Ingrese el valor de la segunda venta'))
-        let pago3 = Number(prompt('Ingrese el valor de la tercera venta'))
-        let sumaVentas = (pago1+pago2+pago3)
-        let extra = sumaVentas*0.10
-        let comisionVal = extra+sumaVentas
-        console.warn(`La comision del vendedor "${n}" es de "${comisionVal}" mas su salario base...`)    
+    //8. Diseñar un pseudocódigo que calcule el promedio ponderado para alumno del ITT. El cálculo se hace
+    // de la siguiente forma:
+    // Se multiplica cada calificación por los créditos de cada materia
+    // El resultado anterior se suma con los resultados de todas las materias, por separado se suman los
+    // créditos de cada materia y finalmente se divide la suma de todas las materias por sus respectivos
+    // créditos, entre la suma de todos los créditos. (materias: Fundamentos, BD y ética).
+let n = 0
+let cantEstudiantes = Number(prompt('ingrese la cantidad de estudiantes a la cual va evaluar el ponderado'))
+while (n<cantEstudiantes) {
+    n++
+    let creditos1 = Number(prompt('Ingrese los creditos de la materia "Fundamentos"'))
+    let creditos2 = Number(prompt('Ingrese los creditos de la materia "BD" '))
+    let creditos3 = Number(prompt('Ingrese los creditos de la materia "Etica"'))
+    let calificacion1 = Number(prompt('Ingrese la calificacion de la materia "Fundamentos"'))
+    let calificacion2 = Number(prompt('Ingrese la calificacion de la materia "BD"'))
+    let calificacion3 = Number(prompt('Ingrese la calificacion de la materia "Etica"'))
+    let mul1 =calificacion1*creditos1 
+    let mul2 = calificacion2*creditos2
+    let mul3 =calificacion3*creditos3
+    let sumMaterias1 = mul1+ calificacion1
+    let sumMaterias2 = mul2+calificacion2
+    let sumMaterias3 = mul3+calificacion3
+    let valMaterias = sumMaterias1+sumMaterias2+sumMaterias3
+    let sumCreditos = creditos1+creditos2+creditos3
+    let ponderado = valMaterias/sumCreditos
+    console.warn(`El promedio de su ponderado de 3 materias, segun las notas ingresadas es de ${ponderado}`)
 }
 })
