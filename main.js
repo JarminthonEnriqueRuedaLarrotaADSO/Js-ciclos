6844860404c04626e869b0d301181c83355a82ce
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-28 21:33:27 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 addEventListener("DOMContentLoaded",()=>{
-    //6. Un avión que viaja 800 Km/hr. Dispara un proyectil auto impulsado, en el momento del disparo, el avión hace un giro de 90 grados y acelera a 20 mtrs/seg2. El proyectil sigue su curso, acelerando a 10 mtrs./seg2. Diseñe un pseudocódigo que escriba cada segundo, la distancia que separa al avión del
-    // proyectil, hasta que estén a 10,000 mtrs. o más.
-    let m = 20
-    let n = 10
-    var s = 0
-    var z = 0
-    while (n < 10001) {
-        n+=10
-        s += 1
-
-    }
-    while (m < 10001) {
-        m+=20
-        z += 1
-
-    }
-    console.log(`El tiempo que tarda el avion en alcanzar 10km de separacion del proyecil son "${s}" segundos`);
-    console.log(`El tiempo que tarda el proyectil en alcanzar 10km de separacion del avion son "${z}" segundos`);
+    //7. Una compañía de seguros tiene contratados a n vendedores. Cada uno hace tres ventas a la semana. Su política de pagos es que un vendedor recibe un sueldo base, y un 10% extra por comisiones de
+    // sus ventas.El gerente de su compañía desea saber cuanto dinero obtendrá en la semana cada vendedor por concepto de comisiones por las tres ventas realizadas, y cuanto tomando en cuenta su sueldo base y sus comisiones.
+    let n1 = Number(prompt('ingrese le numero de vendedores '))
+    let n = 0
+    while (n<n1) {
+        n++
+        let pago1 = Number(prompt('Ingrese el valor de la primera venta '))
+        let pago2 = Number(prompt('Ingrese el valor de la segunda venta'))
+        let pago3 = Number(prompt('Ingrese el valor de la tercera venta'))
+        let sumaVentas = (pago1+pago2+pago3)
+        let extra = sumaVentas*0.10
+        let comisionVal = extra+sumaVentas
+        console.warn(`La comision del vendedor "${n}" es de "${comisionVal}" mas su salario base...`)    
+}
 })
