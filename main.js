44839978c2bca19bf6ea7105af13ca0fd91646c6
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-28 20:58:16 -0500

    Ejercicio 5

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 addEventListener("DOMContentLoaded",()=>{
-    // 4. 42 personas toman parte de un baile. Durante la fiesta una dama bailó con 7 caballeros, una segunda
-    // dama con 8, una tercera con 9, y así sucesivamente hasta que la última bailó con todos los hombres.
-    // Cuantas damas había en el baile?
-    var n = 6
-    while (n < 42){
+    //5. El profesor de una materia desea conocer la cantidad de sus alumnos que no tienen derecho al examen
+    // de nivelación. Diseñe un pseudocódigo que lea las calificaciones obtenidas en las 5 unidades por cada
+    // uno de los 40 alumnos y escriba la cantidad de ellos que no tienen derecho al examen de nivelación.
+    let  n = 0
+    let nivelacion = 0
+    var promedio
+    console.warn('La nota se define segun si obtuvo mas de 3 pasa y no merece hacer el examen de lo contrario si obtuvo menos de 3 tiene que hacer el examen.');
+    while (n<50){
         n++
-        console.log(`n`)
-        
+    let nota1 = Number(prompt(`Alumno ${n} Ingrese sus notas para calcular si tiene derecho a la nivelacion`))
+    let nota2 = Number(prompt(`Alumno ${n} Ingrese sus notas para calcular si tiene derecho a la nivelacion`))
+    let nota3 = Number(prompt(`Alumno ${n} Ingrese sus notas para calcular si tiene derecho a la nivelacion`))
+    let nota4 = Number(prompt(`Alumno ${n} Ingrese sus notas para calcular si tiene derecho a la nivelacion`))
+    let nota5 = Number(prompt(`Alumno ${n} Ingrese sus notas para calcular si tiene derecho a la nivelacion`))
+    nivelacion = nota1+nota2+nota3+nota4+nota5
+    promedio = nivelacion/5
+    if (promedio<3) {
+        console.log(`Alumno ${n} Tiene derecho al examen de nivelacion "ut ha perdido  :(" `)
+    }    
+    else{
+        console.log(`Alumno ${n} No tiene derecho al examen de nivelacion "ut ha pasado :) " `)
     }
-    let numeroPersonas = 36/2
-    let caballeros = 42 - numeroPersonas
-    console.warn(`El numero de mujeres es  "${numeroPersonas}" y el numero de caballeros es "${caballeros}"`)
+    }
 })
